d06f04df71adc2f42b0b5acc2736a8fd3d80ebe8
cheater <cheater@pterodactyl.(none)>
2009-10-10 17:12:51 +1100

    Forgot the extra table

--- import/osmimport.sql
+++ import/osmimport.sql
@@ -15,7 +15,8 @@ CREATE TABLE IF NOT EXISTS `nodes` (
 CREATE TABLE IF NOT EXISTS `nodes_to_ways` (
   `node_id` int(10) unsigned NOT NULL,
   `way_id` int(10) unsigned NOT NULL,
-  `order` int(10) unsigned NOT NULL
+  `order` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`node_id`,`way_id`,`order`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tags` (
@@ -42,3 +43,10 @@ CREATE TABLE IF NOT EXISTS `ways` (
   `way_version` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY  (`way_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ways_intersections` (
+  `way_1` int(10) unsigned NOT NULL,
+  `way_2` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`way_1`,`way_2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
