6c86b791b9d62bd2a26c0c68637c79c4c144d9d2
cheater <cheater@pterodactyl.(none)>
2009-10-10 15:45:27 +1100

    Now lets pre-calculate all the intersections. :)

---
+++ import/calculate_intersections.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+*
+* Intersections Calculator
+*
+* Created by Nick Stallman <nick@nickstallman.net>
+* 
+*/
+
+
+// Settings
+
+$set_osm_filename = 'uk-090930.osm';
+$set_db_server = ':/var/run/mysqld/mysqld.sock';   // use a socket if on localhost! location varies, see socket setting in mysql's my.conf
+$set_db_user = 'root';
+$set_db_pass = '';
+$set_db_db = '';
+$set_log_interval = 100000;
+
+// End of settings
+
+$way_cache = array();
+
+$result = mysql_query('SELECT node_id FROM nodes');
+$count = mysql_num_rows($result);
+$done = 0;
+
+while ($row = mysql_fetch_array($result)
+{
+	$done++;
+
+	$result2 = mysql_query('SELECT way_id FROM nodes_to_ways WHERE node_id = '.$row['node_id']);
+	if (mysql_num_rows($result2) > 1)
+	{
+		$ways = array();
+		while ($row2 = mysql_fetch_array($result2))
+		{
+			$ways[] = $row2['way_id'];
+		}
+		sort($ways);
+
+		$c = count($ways);
+		for ($i = 0; $i < $c; $i++)
+		{
+			for ($j = $i + 1; $j < $c; $j++)
+			{
+				$way_cache[$ways[$i]][$ways[$j]] = 1;
+			}
+		}
+	}
+}
+
+$inserts = array();
+$inserts_count = 0;
+foreach ($way_cache as $way_1 => $ways)
+{
+	foreach ($ways as $way_2 => $num)
+	{
+		$inserts[] = "($way_1, $way_2)";
+		$inserts_count++;
+	}
+
+	if ($inserts_count > 10000)
+	{
+		mysql_query('INSERT INTO intersections VALUES '.implode(',', $inserts));
+		$inserts = array();
+		$inserts_count = 0;
+	}
+}
+
+mysql_query('INSERT INTO intersections VALUES '.implode(',', $inserts));
